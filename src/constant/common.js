ee84f4e19313e2d1abf6cec670c7cf84305888c1
blackmeender <same.98@mail.ru>
2022-02-24 01:04:18 +1100

    hw3

--- src/constant/common.js
+++ src/constant/common.js
@@ -1,4 +1,6 @@
+import { ArrowUpward, Android, SentimentSatisfiedAlt } from '@mui/icons-material';
+import React, { useEffect, useState } from 'react';
 export const AUTHOR = {
-    me: 'me',
-    bot: 'bot'
+    me: <SentimentSatisfiedAlt />,
+    bot: <Android />
 }
